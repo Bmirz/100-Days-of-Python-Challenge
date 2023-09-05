e7e52ea2fa51ec779b94c6398aa63223a95f591f
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-05 11:19:53 -0500

    Create data-types.py
    Simple data types

---
+++ day2/data-types.py
@@ -0,0 +1,20 @@
+#Data Types
+
+#String
+
+print("Hello"[4]) # 0 - H, 1 - e, 2 - l, etc.
+
+print("123" + "456") #just concatonates
+
+#Integer
+
+print(123 + 456)
+
+#Float
+
+print(3.145259)
+
+#Boolean
+
+True
+False
