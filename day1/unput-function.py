dcbdbd97443a81614917b7637624a67a105c3971
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-05 00:26:55 -0500

    Create unput-function.py
    exercise to teach the input python function

---
+++ day1/unput-function.py
@@ -0,0 +1,11 @@
+
+print ("Hello")
+
+
+
+color = input("What is your favorite color? ")
+
+pet = input("What is your favorite type of pet? ")
+
+
+print("Then name of your band is " + color + " " + pet + "." )
