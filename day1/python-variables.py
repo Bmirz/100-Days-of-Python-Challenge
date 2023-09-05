35eb57172ad439ad39b7f7d42815d274ce04aef0
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-05 11:00:30 -0500

    Update python-variables.py

--- day1/python-variables.py
+++ day1/python-variables.py
@@ -7,3 +7,23 @@ name = input("What is your name?")
 lenght = len(name)
 
 print(length)
+
+# *** Exercise ***
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temp = a
+a = b
+b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
