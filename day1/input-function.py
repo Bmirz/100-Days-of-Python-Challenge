b83744240d9aab53819f3ba669d2770c1ce63164
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-05 00:35:14 -0500

    Update input-function.py

--- day1/input-function.py
+++ day1/input-function.py
@@ -1,11 +1,9 @@
+# input() will get user input in console
+# print() will print hard coded text to console
 
-print ("Hello")
+ 
+print ("Hello " + input("what is your name?"))
 
+# len() finds the length of a string and assigns a number
 
-
-color = input("What is your favorite color? ")
-
-pet = input("What is your favorite type of pet? ")
-
-
-print("Then name of your band is " + color + " " + pet + "." )
+print( len( input("what is your name?) ) )
