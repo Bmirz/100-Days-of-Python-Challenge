dfd008ccf9ff8c62527e8108f345600d1e476c45
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-04 23:16:06 -0500

    Day 1
    Simple print statements

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print ("Hello")
+
+
+
+color = input("What is your favorite color? ")
+
+pet = input("What is your favorite type of pet? ")
+
+
+print("Then name of your band is " + color + " " + pet + "." )
+
