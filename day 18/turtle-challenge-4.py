f508ae21b03bde26249814cdf645bc4bafd62ed5
Bilal Mirza <125304028+Bmirz@users.noreply.github.com>
2023-09-26 18:01:37 -0500

    Update turtle-challenge-4.py

--- day 18/turtle-challenge-4.py
+++ day 18/turtle-challenge-4.py
@@ -7,16 +7,25 @@ import random
 timmy_turtle = t.Turtle()
 timmy_turtle.shape("turtle")
 screen = Screen()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180 , 270]
 timmy_turtle.pensize(10)
 timmy_turtle.speed("fastest")
 
 for index in range (250):
-    timmy_turtle.color(random.choice(colours))
+    timmy_turtle.color(random_color())
     timmy_turtle.forward(30)
     timmy_turtle.setheading(random.choice(directions))
+
+timmy_turtle.reset()
 
 screen.exitonclick()
