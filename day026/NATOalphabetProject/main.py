a26e3dddb28a2f8835a10807fd97b60ecefb89d6
Bilal Mirza <bbmirza117@gmail.com>
2023-10-07 02:40:27 -0500

    Completed Day 26

--- day026/NATOalphabetProject/main.py
+++ day026/NATOalphabetProject/main.py
@@ -1,27 +1,28 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+data = pandas.read_csv("day026/NATOalphabetProject/nato_phonetic_alphabet.csv")
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# {"A": "Alfa", "B": "Bravo"}
+
+# #My Solution
+# phonetic_dict = {}
+# #Loop through rows of a data frame
+# for (index, row) in data.iterrows():
+#     phonetic_dict.update({row.letter: row.code})
+# # print(phonetic_dict)
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+user_input = input("Input word you would like phonetically spelled out: ").upper()
+
+# #My Solution
+# for char in user_input:
+#     for (key, value) in phonetic_dict.items():
+#         if key == char:
+#             print(f"{value}")
+
+output_list = [phonetic_dict[letter] for letter in user_input]
+print(output_list)
